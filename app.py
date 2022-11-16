de08983a5022337835c3dce615812309b8d12a9a
Hacksenn <gurcks2922@gmail.com>
2022-11-16 11:23:26 +0900

    메인페이지구현

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
-print('app.py 입니다.')
+from flask import Flask, render_template, jsonify, request
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.rypuzfe.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbMyPlaylist
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/detail')
+def detail():
+    return render_template("detail.html")
+
+@app.route("/api/posts", methods=['GET'])
+def get_posts():
+    posts = list(db.sing.find({}).sort("date", -1).limit(20))
+    for post in posts:
+        post["_id"] = str(post["_id"])
+    return jsonify({"result": "success", "msg": "카드를 가져왔습니다.", "posts": posts})
+
+#comment post
+@app.route("/comment", methods=["POST"])
+def comments_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.comments.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+#comment get
+@app.route("/comment", methods=["GET"])
+def comments_get():
+    comment_list = list(db.comments.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
