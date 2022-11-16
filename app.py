c7e21f84428523c9b3871cfc8177f223588c0a0d
gPwn <hyeju3014@gmail.com>
2022-11-16 20:14:43 +0900

    detail 수정

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.rypuzfe.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbMyPlaylist
+
+
+@app.route("/api/posts", methods=["POST"])
+def web_comments_post():
+    img_receive = request.form['img_give']
+    title_receive = request.form['title_give']
+    singer_receive = request.form['singer_give']
+    comment_receive = request.form['comment_give']
+    genre_receive = request.form['genre_give']
+    date_receive = request.form['date_give']
+
+    all_sings = list(db.sing.find({}, {'_id': False}))
+    count = len(all_sings) + 1
+
+    doc = {
+        'img': img_receive,
+        'title': title_receive,
+        'singer': singer_receive,
+        'comment': comment_receive,
+        'genre': genre_receive,
+        'date': date_receive,
+        'index': count
+
+    }
+
+    db.sing.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/detail')
-def detail():
-    return render_template("detail.html")
+
+@app.route('/create')
+def create():
+    return render_template('creates.html')
+
 
 @app.route("/api/posts", methods=['GET'])
 def get_posts():
@@ -20,14 +53,8 @@ def get_posts():
         post["_id"] = str(post["_id"])
     return jsonify({"result": "success", "msg": "카드를 가져왔습니다.", "posts": posts})
 
-#sing get_detail
-@app.route("/detail/sing", methods=["GET"])
-def sing_get():
-    id = int(request.args('index'))
-    sing_one = db.sing.find_one({'index':id}, {'_id': False})
-    return jsonify({'sings': sing_one})
 
-#comment post
+# comment post
 @app.route("/comment", methods=["POST"])
 def comments_post():
     name_receive = request.form['name_give']
@@ -41,13 +68,27 @@ def comments_post():
 
     return jsonify({'msg': '저장 완료!'})
 
-#comment get
+
+# comment get
 @app.route("/comment", methods=["GET"])
 def comments_get():
     comment_list = list(db.comments.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
+
+@app.route("/detail/")
+def get_cards():
+    one_card = request.args.get('card')
+    print(one_card)
+    cards = db.sing.find_one({'index': int(one_card)}, {'_id': False})
+    if cards:
+        img = cards['img']
+        title = cards['title']
+        singer = cards['singer']
+        genre = cards['genre']
+        comment = cards['comment']
+    return render_template("detail.html", img=img, title=title, singer=singer, genre=genre, comment=comment)
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-    # dklfjlskjdf
--- static/assets/js/list.js
+++ static/assets/js/list.js
@@ -1,6 +1,7 @@
 
 
 
+
 function get_posts() {
     $.ajax({
         type: "GET",
@@ -19,7 +20,7 @@ function get_posts() {
                 let index = post['index']
                 let time_post = new Date(post['date'])
                 let time_before = time2str(time_post)
-                let temp_html = `<div id="" class="card" onclick="window.location.href = \`/detail?id=${index}\`">
+                let temp_html = `<div id="" class="card" onclick="window.location.href = \`/detail/?card=${index}\`">
                                     <div class="card-image">
                                         <figure class="image is-square">
                                             <img src="${img}" alt="Placeholder image">
@@ -59,4 +60,3 @@ function time2str(date) {
     }
     return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`
 }
-
