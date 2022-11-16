54474ded0517bec9fe6eba64416ecd6572672756
CHUCHUDACHUCHU <116955269+CHUCHUDACHUCHU@users.noreply.github.com>
2022-11-16 15:19:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.rypuzfe.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbMyPlaylist
 
-<<<<<<< HEAD
-
-@app.route('/')
-def home():
-   return render_template('creates.html')
-
 @app.route("/api/posts", methods=["POST"])
 def web_comments_post():
     img_receive = request.form['img_give']
@@ -37,8 +31,7 @@ def web_comments_post():
     db.sing.insert_one(doc)
 
     return jsonify({'msg': '등록 완료!'})
-
-=======
+    
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -46,6 +39,10 @@ def home():
 @app.route('/detail')
 def detail():
     return render_template("detail.html")
+    
+@app.route('/create')
+def create():
+   return render_template('creates.html')
 
 @app.route("/api/posts", methods=['GET'])
 def get_posts():
@@ -79,7 +76,6 @@ def comments_get():
 def sing_get():
     sing_one = db.sing.find_one({'index':'1'}, {'_id': False})
     return jsonify({'sings': sing_one})
->>>>>>> develop
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
